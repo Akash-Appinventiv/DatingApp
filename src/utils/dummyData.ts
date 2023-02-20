339f6dcda1b5d9ec77fe89e65dd8af922eec99b4
Akash-Appinventiv <akash.agrawal@appinventiv.com>
2023-02-20 16:28:05 +0530

    added walkthrough screen

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,22 @@
+import localimages from './localimages';
+
+export const walthroughImages = [
+  localimages.WALKTHROUGH1,
+  localimages.WALKTHROUGH2,
+  localimages.WALKTHROUGH1,
+];
+
+export const walkthroughText = [
+  {
+    title: 'Algorithm',
+    body: 'Users going through a vetting process to ensure you never match with bots.',
+  },
+  {
+    title: 'Matches',
+    body: 'We match you with people that have a large array of similar interests.',
+  },
+  {
+    title: 'Premium',
+    body: 'Sign up today and enjoy the first month of premium benefits on us.',
+  },
+];
---
+++ src/utils/localimages.ts
@@ -0,0 +1,6 @@
+const DEFAULTPATH = '../assets/images/';
+
+export default {
+  WALKTHROUGH1: require(`${DEFAULTPATH}walkthrough1.png`),
+  WALKTHROUGH2: require(`${DEFAULTPATH}walkthrough2.png`),
+};
