32276dede23134452beedfd496ea14db065d38f7
AbhishekOjha7 <abhishek.ojha@appinventiv.com>
2023-02-20 16:36:07 +0530

    signUP

---
+++ src/components/TouchableImage.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+
+const TouchableImage = () => {
+  return (
+    <TouchableOpacity>
+      <Text>TouchableImage</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TouchableImage;
+
+const styles = StyleSheet.create({});
---
+++ src/utils/localimages.ts
@@ -0,0 +1,6 @@
+export const images = {
+  logo: require('../assets/images/logo.png'),
+  Fb: require('../assets/images/Fb.png'),
+  Google: require('../assets/images/Google.png'),
+  Apple: require('../assets/images/Apple.png'),
+};
